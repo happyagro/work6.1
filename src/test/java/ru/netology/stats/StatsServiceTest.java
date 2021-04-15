08509e628eebdc120a5770e9dd6810ab964ffb0a
Nikita Aniskovets <happyaggro@gmail.com>
2021-04-15 12:55:31 +0300

    add logic

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StatsServiceTest {
+
+    @Test
+    void statisticSaleSum() {
+    }
+
+    @Test
+    void statisticSaleAverageMonth() {
+    }
+
+    @Test
+    void statisticSaleMax() {
+    }
+
+    @Test
+    void statisticSaleMin() {
+    }
+
+    @Test
+    void statisticMonthUnderAverage() {
+    }
+
+    @Test
+    void statisticMonthOverAverage() {
+    }
+}
